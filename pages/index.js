2e863f8ef539e64ce8dd55d15d02d528ffce3f07
muanra <muhamaddr@upnvj.ac.id>
2022-08-31 22:08:52 +0700

    initial next js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>AM Donat</title>
+        <meta name="description" content="Best UMKM Food in Town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Homepage
+    </div>
+  )
+}
