2d32381d1f91e6e0aa116728c9d165dc1f5b1aa2
muanra <muhamaddr@upnvj.ac.id>
2022-09-01 11:23:55 +0700

    initial navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src={"/img/telephone.png"} alt="callButton" width="32" height="32"/>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>0857 1109 7085</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src={"/img/logo.png"} alt="logo" width="160px" height="69px"/>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src={"/img/cart.png"} alt="logo" width="30px" height="30px"/>
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
