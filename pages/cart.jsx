fd21d3042475037452818b1c55057b4751688b8f
muanra <muhamaddr@upnvj.ac.id>
2022-09-08 14:16:07 +0700

    initial responsive

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,86 +6,88 @@ const Cart = () => {
     <div className={styles.container}>
         <div className={styles.left}>
             <table className={styles.table}>
-                <tr className={styles.tr}>
-                    <th>Product</th>
-                    <th>Name</th>
-                    <th>Extras</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={styles.imgContainer}>
-                            <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>PEPPERONI</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>
-                            Double ingredient, spicy sauce
-                        </span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>Rp.10000</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>2</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>Rp.20000</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={styles.imgContainer}>
-                            <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>PEPPERONI</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>
-                            Double ingredient, spicy sauce
-                        </span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>Rp.10000</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>2</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>Rp.20000</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={styles.imgContainer}>
-                            <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>PEPPERONI</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>
-                            Double ingredient, spicy sauce
-                        </span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>Rp.10000</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>2</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>Rp.20000</span>
-                    </td>
-                </tr>
+                <tbody>
+                    <tr className={styles.trTitle}>
+                        <th>Product</th>
+                        <th>Name</th>
+                        <th>Extras</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>PEPPERONI</span>
+                        </td>
+                        <td>
+                            <span className={styles.extras}>
+                                Double ingredient, spicy sauce
+                            </span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>Rp.10000</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>Rp.20000</span>
+                        </td>
+                    </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>PEPPERONI</span>
+                        </td>
+                        <td>
+                            <span className={styles.extras}>
+                                Double ingredient, spicy sauce
+                            </span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>Rp.10000</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>Rp.20000</span>
+                        </td>
+                    </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image src="/img/pizza.png" layout='fill' objectFit='cover' alt='pizza' />
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>PEPPERONI</span>
+                        </td>
+                        <td>
+                            <span className={styles.extras}>
+                                Double ingredient, spicy sauce
+                            </span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>Rp.10000</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>Rp.20000</span>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
         <div className={styles.right}>
