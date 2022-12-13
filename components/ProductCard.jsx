da85c91828f51ab29027b2c49588688805c4a02f
muanra <muhamaddr@upnvj.ac.id>
2022-12-13 11:31:48 +0700

    added link to cardProduct's title

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -8,7 +8,9 @@ const ProductCard = ({product}) => {
       <Link href={`/product/${product._id}`} passHref>
         <a><Image src={product.img} width={500} height={500} alt="pizza"/></a>
       </Link>
-      <h1 className={styles.title}>{product.title}</h1>
+      <Link href={`/product/${product._id}`} passHref>
+        <h1 className={styles.title}>{product.title}</h1>
+      </Link>
       <span className={styles.price}>{product.prices[0].toLocaleString("id-ID", {style:"currency", currency:"IDR"})}</span>
       <p className={styles.desc}>{product.desc}</p>
     </div>
